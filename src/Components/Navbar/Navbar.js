ff0978551501b455eea7a54cdaa26c16c32d2f40
Lazarus Jr <96751425+LazarusJr@users.noreply.github.com>
2022-03-04 15:38:13 +0000

    Add: Navbar

---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import Logo from '../../Assets/logo.png'
+import './Navbar.css'
+import {GiHamburgerMenu} from 'react-icons/gi'
+import {CgCloseR} from 'react-icons/cg'
+
+const Navbar = () => {
+
+    const [click, clicked] = useState(false)
+    const burgerClick = () => clicked(!click)
+
+  return (
+    <div className='navbar'>
+    <div className='container'>
+        <div className='text-logo'>
+        <h1>XEFI</h1>
+        </div>
+        <div className='logo'>
+        <img src={Logo} alt='' />
+       </div>
+           <ul className='nav'>
+               <li className='nav-item'><a href='/'>Home</a></li>
+               <li className='nav-item'><a href='/'>About</a></li>
+               <li className='nav-item'><a href='./'>FAQ</a></li>
+               <li className='nav-item'><a href='./'>Contact Us</a></li>
+               <button className='btn-burger'>SIGN UP</button>
+           </ul>
+           <div className='hamburger' onClick={burgerClick}>
+               {click ? (<CgCloseR size={30} />) : (<GiHamburgerMenu size={30} />)}
+           </div> 
+       </div>
+    </div>      
+  )
+}
+
+export default Navbar
